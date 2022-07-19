93e1dd66f7bf92d45901f33ef899e56d9203b346
ecdavis97 <ethancdavis97@gmail.com>
2022-07-19 15:13:42 -0400

    init commit

--- js/site.js
+++ js/site.js
@@ -1,17 +1,30 @@
+//controller function
 function getValue(){
-    alert("Hello World!")
+    message = document.getElementById("message").value;
+    let revMessage = reverse(message);
+    displayReversed(revMessage);
 }
 
-function getValue2(){
-    let msg='';
 
-    msg=document.getElementById("message").value;
+//business logic
+function reverse(message){
+    //string? an array of character
+    let reversedMessage = "";
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
-        }
-    );
+    //decrementing for loop
+    for (let index = message.length-1; index >=0; index--) {
+        
+        reversedMessage += message[index];
+        
+    }
+
+    return reversedMessage;
+
+}
+
+//display the reversed string 
+function displayReversed(revMessage){
+    let results = document.getElementById("results");
+    results.classList.add("revMessage"); //this adds the ability to change the style through CSS
+    results.innerHTML = revMessage;
 }
